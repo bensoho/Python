44d0c2886aeeb69e226edf68805260587e4a7639
bensoho <bliu7@foxmail.com>
2018-10-10 23:03:24 +0800

    done

--- WebSpider/test02.py
+++ WebSpider/test02.py
@@ -4,14 +4,11 @@ import sys
 text = '''
         <div class="nav">
             <ul class="navbar">
-                <li><a href="/" data-act="home-click"  >Home</a></li>
-                <li><a href="/films" data-act="movies-click" >电影</a></li>
-                <li><a href="/cinemas" data-act="cinemas-click" >影院</a></li> 
-                <li><a href="/board" data-act="board-click"  class="active" >榜单</a></li>
-                <li><a href="/news" data-act="hotNews-click" >热点</a></li>
+                <li class="li li-first" name="item"><a href="/" data-act="home-click">首页</a></li>
             </ul>
         </div>
         '''
-html = etree.parse('./test.html',etree.HTMLParser(encoding='utf-8'))
-result = html.xpath('//li/a/text()')
+# html = etree.parse('./test.html',etree.HTMLParser(encoding='utf-8'))
+html = etree.HTML(text)
+result = html.xpath('//li[contains(@class,"li") and @name="item"]/a/text()')
 print(result)
